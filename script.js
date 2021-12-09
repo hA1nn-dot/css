f9c1031086de1447033f30812cdaa4d56a5f75d8
Haim Guel Quiroz <hgq_2000@hotmail.com>
2021-12-09 00:34:30 -0600

    Fix updating score

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var guessingNumber = Math.trunc(Math.random() * 20) + 1;
+var score = 20;
+updateScore();
 // var turns = document.querySelector("");
 document
   .querySelector(".grid-input-number__button")
@@ -17,8 +19,15 @@ document
     } else if (guessingNumber > number) {
       document.querySelector(".message").textContent =
         "It's greater than " + number;
+      score--;
     } else {
       document.querySelector(".message").textContent =
         "It's less than " + number + ";)";
+      score--;
     }
+    updateScore();
   });
+
+function updateScore() {
+  document.querySelector(".score_text").textContent = score;
+}
