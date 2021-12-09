5f385bf85d802804ac35563fd6233355092d2e35
Haim Guel Quiroz <hgq_2000@hotmail.com>
2021-12-09 00:25:12 -0600

    Check and verify number and customize text styles

---
+++ script.js
@@ -0,0 +1,24 @@
+var guessingNumber = Math.trunc(Math.random() * 20) + 1;
+// var turns = document.querySelector("");
+document
+  .querySelector(".grid-input-number__button")
+  .addEventListener("click", () => {
+    let number = Number(document.querySelector(".number__input").value);
+
+    console.log(number, typeof number);
+    if (!number) {
+      document.querySelector(".message").textContent = "It's not a number! xc";
+      return;
+    }
+
+    if (number === guessingNumber) {
+      document.querySelector(".numberValue").textContent = guessingNumber;
+      document.querySelector(".message").textContent = "Correct Number!";
+    } else if (guessingNumber > number) {
+      document.querySelector(".message").textContent =
+        "It's greater than " + number;
+    } else {
+      document.querySelector(".message").textContent =
+        "It's less than " + number + ";)";
+    }
+  });
