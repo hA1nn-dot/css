e571a2779a90a1d933b7b140131acf4a092b37d9
Haim Guel Quiroz <hgq_2000@hotmail.com>
2021-12-10 11:56:16 -0600

    Clean code 1

--- script.js
+++ script.js
@@ -2,52 +2,61 @@ var guessingNumber = Math.trunc(Math.random() * 20) + 1;
 var score = 20;
 var highscore = 0;
 updateScore();
-// var turns = document.querySelector("");
+
 document
   .querySelector(".grid-input-number__button")
   .addEventListener("click", () => {
     let number = Number(document.querySelector(".number__input").value);
 
     if (!number) {
-      document.querySelector(".message").textContent = "It's not a number! xc";
+      showMessage("It's not a number! xc");
       return;
     }
 
     if (number === guessingNumber) {
+      showMessage("Correct Number!");
+      updateHighScore();
       document.querySelector(".numberValue").textContent = guessingNumber;
-      score > highscore ? (highscore = score) : (highscore = highscore);
-      document.querySelector(".highscore").textContent = highscore;
-      document.querySelector(".message").textContent = "Correct Number!";
       document.querySelector(".body").classList.add("win");
-    } else if (guessingNumber > number) {
-      document.querySelector(".message").textContent =
-        "It's greater than " + number;
-      score--;
     } else {
-      document.querySelector(".message").textContent =
-        "It's less than " + number + " ;)";
+      showMessage(
+        guessingNumber > number
+          ? "It's greater than " + number
+          : "It's less than " + number + " ;)"
+      );
       score--;
     }
 
     updateScore();
     if (isPlayerLost()) {
-      document.querySelector(".message").textContent = "You lost the game :c";
+      showMessage("You lost the game :c");
       document.querySelector(".numberValue").textContent = guessingNumber;
     }
   });
 document
   .querySelector(".header-button__retry")
   .addEventListener("click", () => {
+    score = 20;
+    showMessage("Start guessing...");
+    guessingNumber = Math.trunc(Math.random() * 20) + 1;
     document.querySelector("body").classList.remove("win");
     document.querySelector(".numberValue").textContent = "?";
-    document.querySelector(".message").textContent = "Start guessing...";
     document.querySelector(".score_text").textContent = "20";
-    score = 20;
-    guessingNumber = Math.trunc(Math.random() * 20) + 1;
   });
+
+function updateHighScore() {
+  if (score > highscore) {
+    highscore = score;
+    document.querySelector(".highscore").textContent = highscore;
+  }
+}
 
 function updateScore() {
   document.querySelector(".score_text").textContent = score;
+}
+
+function showMessage(message) {
+  document.querySelector(".message").textContent = message;
 }
 
 function isPlayerLost() {
